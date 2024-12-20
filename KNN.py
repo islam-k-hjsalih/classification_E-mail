cac8d634fc394a359538cc8e590e95a682ccc6cd
islam-k-hjsalih <i.k.hjsalih@students.ptuk.edu.ps>
2024-12-20 21:50:48 +0200

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -50,3 +50,39 @@ error_rate = (fp + fn) / (tp + tn + fp + fn)  # نسبة الخطأ
 
 print(f"نسبة الصواب: {accuracy * 100:.2f}%")
 print(f"نسبة الخطأ: {error_rate * 100:.2f}%")
+
+
+"""
+
+
+*************************###OUTPUT###**************************
+                                                   text  target
+0     From ilug-admin@linux.ie Mon Jul 29 11:28:02 2...       0
+1     From gort44@excite.com Mon Jun 24 17:54:21 200...       1
+2     From fork-admin@xent.com Mon Jul 29 11:39:57 2...       1
+3     From dcm123@btamail.net.cn Mon Jun 24 17:49:23...       1
+4     From ilug-admin@linux.ie Mon Aug 19 11:02:47 2...       0
+...                                                 ...     ...
+5791  From ilug-admin@linux.ie Mon Jul 22 18:12:45 2...       0
+5792  From fork-admin@xent.com Mon Oct 7 20:37:02 20...       0
+5793  Received: from hq.pro-ns.net (localhost [127.0...       1
+5794  From razor-users-admin@lists.sourceforge.net T...       0
+5795  From rssfeeds@jmason.org Mon Sep 30 13:44:10 2...       0
+
+[5796 rows x 2 columns]
+Index(['text', 'target'], dtype='object')
+[[775   4]
+ [ 19 362]]
+              precision    recall  f1-score   support
+
+           0       0.98      0.99      0.99       779
+           1       0.99      0.95      0.97       381
+
+    accuracy                           0.98      1160
+   macro avg       0.98      0.97      0.98      1160
+weighted avg       0.98      0.98      0.98      1160
+
+Accuracy :98.02%
+Error Rate : 1.98%
+
+"""
